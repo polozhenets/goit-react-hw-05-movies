32150dd4744221296c48d9b72558d9951f68ff8a
Kiril Polozhenets <90510727+polokir@users.noreply.github.com>
2023-04-26 12:26:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from 'components/App';
 import './index.css';
 //basename="/goit-react-hw-05-movies"
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter >
+  <BrowserRouter basename="/goit-react-hw-05-movies" >
     <App />
   </BrowserRouter>
 );
